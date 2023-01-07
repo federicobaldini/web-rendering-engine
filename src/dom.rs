ddb9497b84d438b88eec624dac9b577e9046fe57
Federico Baldini <federico.baldini7@gmail.com>
2023-01-07 12:28:23 +0100

    Update dom.rs
    Added an "hasmap" macro to generate faster an hashmap structure.

--- src/dom.rs
+++ src/dom.rs
@@ -78,3 +78,12 @@ impl Node {
     }
   }
 }
+
+#[macro_export]
+macro_rules! hashmap {
+  ($( $key: expr => $val: expr ),*) => {{
+       let mut map = ::std::collections::HashMap::new();
+       $( map.insert($key, $val); )*
+       map
+  }}
+}
