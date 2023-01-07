26a65fcb5b27d3755863c9bbf4d676f59a250444
Federico Baldini <federico.baldini7@gmail.com>
2023-01-07 12:27:47 +0100

    Update dom.rs
    Improved dom structures and definitions.

--- src/dom.rs
+++ src/dom.rs
@@ -1,22 +1,63 @@
+#![allow(dead_code)]
+
 use std::collections::HashMap;
 
-type AttributesMap = HashMap<String, String>;
+pub type AttributesMap = HashMap<String, String>;
 
-struct ElementData {
+#[derive(Clone, Debug)]
+pub struct ElementData {
   tag_name: String,
   attributes: AttributesMap,
 }
 
-enum NodeType {
+impl PartialEq for ElementData {
+  fn eq(&self, other: &Self) -> bool {
+    *self.tag_name == *other.tag_name && self.attributes == other.attributes
+  }
+}
+
+impl ElementData {
+  pub fn new(tag_name: String, attributes: AttributesMap) -> ElementData {
+    ElementData {
+      tag_name,
+      attributes,
+    }
+  }
+  pub fn tag_name(&self) -> String {
+    self.tag_name.clone()
+  }
+  pub fn attributes(&self) -> AttributesMap {
+    self.attributes.clone()
+  }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum NodeType {
   Text(String),
   Element(ElementData),
 }
 
-struct Node {
+#[derive(Clone, Debug)]
+pub struct Node {
   // data common to all nodes:
   children: Vec<Node>,
   // data specific to each node type:
   node_type: NodeType,
+}
+
+impl Node {
+  pub fn children(&self) -> Vec<Node> {
+    self.children.clone()
+  }
+  pub fn node_type(&self) -> NodeType {
+    self.node_type.clone()
+  }
+}
+
+impl PartialEq for Node {
+  fn eq(&self, other: &Self) -> bool {
+    *self.children == *other.children && self.node_type == other.node_type
+  }
 }
 
 impl Node {
@@ -27,11 +68,11 @@ impl Node {
     }
   }
 
-  pub fn element(name: String, attributes: AttributesMap, children: Vec<Node>) -> Node {
+  pub fn element(tag_name: String, attributes: AttributesMap, children: Vec<Node>) -> Node {
     Node {
       children,
       node_type: NodeType::Element(ElementData {
-        tag_name: name,
+        tag_name,
         attributes,
       }),
     }
