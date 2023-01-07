87a32c5a1b8e334f666cee38c24207c00bff86fc
Federico Baldini <federico.baldini7@gmail.com>
2023-01-08 00:01:58 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod dom;
+mod parser;
 
 fn main() {
-  println!("Hello, world!");
+  let _html_text_dom = dom::Node::text("Hello World!".to_string());
+  let _root_dom_node = parser::Parser::parse("<div><span>Hello World!</span></div>".to_string());
 }
