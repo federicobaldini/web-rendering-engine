dd652d9b362136eef4ed8e6383c41f8e661abc66
Federico Baldini <federico.baldini7@gmail.com>
2023-01-08 00:54:23 +0100

    Update dom.rs
    Get a better organization of the dom tests by putting them in specific tests module.

--- src/dom.rs
+++ src/dom.rs
@@ -88,29 +88,34 @@ macro_rules! hashmap {
   }}
 }
 
-// Test the associated function text() of the Node struct implementation.
-#[test]
-fn test_node_text() {
-  let node: Node = Node::text("Hello World!".to_string());
+#[cfg(test)]
+mod tests {
+  use crate::dom::*;
 
-  assert_eq!(node.children(), Vec::new());
-  match node.node_type() {
-    NodeType::Text(data) => assert_eq!(data, "Hello World!".to_string()),
-    _ => panic!("Unexpected node type"),
+  // Test the associated function text() of the Node struct implementation.
+  #[test]
+  fn test_node_text() {
+    let node: Node = Node::text("Hello World!".to_string());
+
+    assert_eq!(node.children(), Vec::new());
+    match node.node_type() {
+      NodeType::Text(data) => assert_eq!(data, "Hello World!".to_string()),
+      _ => panic!("Unexpected node type"),
+    }
   }
-}
 
-// Test the associated function element() of the Node struct implementation.
-#[test]
-fn test_node_element() {
-  let tag_name: String = String::from("p");
-  let attributes: AttributesMap = hashmap![String::from("class") => String::from("paragraph")];
-  let children: Vec<Node> = vec![];
-  let node: Node = Node::element(tag_name.clone(), attributes.clone(), children.clone());
+  // Test the associated function element() of the Node struct implementation.
+  #[test]
+  fn test_node_element() {
+    let tag_name: String = String::from("p");
+    let attributes: AttributesMap = hashmap![String::from("class") => String::from("paragraph")];
+    let children: Vec<Node> = vec![];
+    let node: Node = Node::element(tag_name.clone(), attributes.clone(), children.clone());
 
-  assert_eq!(
-    node.node_type(),
-    NodeType::Element(ElementData::new(tag_name, attributes))
-  );
-  assert_eq!(node.children(), children);
+    assert_eq!(
+      node.node_type(),
+      NodeType::Element(ElementData::new(tag_name, attributes))
+    );
+    assert_eq!(node.children(), children);
+  }
 }
