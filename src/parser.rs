34387e594e6e9ae7b7495b37d3e1db6da0e0c05c
Federico Baldini <federico.baldini7@gmail.com>
2023-01-12 18:12:57 +0100

    Update parser.rs
    Update comments.

--- src/parser.rs
+++ src/parser.rs
@@ -7,7 +7,7 @@ pub struct Parser {
 }
 
 impl Parser {
-  // Read the current character without consuming it.
+  // Read the current character without consuming it
   fn next_char(&self) -> char {
     self.input[self.position..].chars().next().unwrap()
   }
@@ -17,12 +17,12 @@ impl Parser {
     self.input[self.position..].starts_with(s)
   }
 
-  // Return true if all input is consumed.
+  // Return true if all input is consumed
   fn eof(&self) -> bool {
     self.position >= self.input.len()
   }
 
-  // Return the current character, and advance self.position to the next character.
+  // Return the current character, and advance self.position to the next character
   fn consume_char(&mut self) -> char {
     let mut iter = self.input[self.position..].char_indices();
     let (_, current_char) = iter.next().unwrap();
@@ -31,7 +31,7 @@ impl Parser {
     return current_char;
   }
 
-  // Consume characters until `test` returns false.
+  // Consume characters until `test` returns false
   fn consume_while<F>(&mut self, test: F) -> String
   where
     F: Fn(char) -> bool,
@@ -43,12 +43,12 @@ impl Parser {
     return result;
   }
 
-  // Consume and discard zero or more whitespace characters.
+  // Consume and discard zero or more whitespace characters
   fn consume_whitespace(&mut self) {
     self.consume_while(char::is_whitespace);
   }
 
-  // Parse a tag or attribute name.
+  // Parse a tag or attribute name
   fn parse_tag_name(&mut self) -> String {
     self.consume_while(|c| match c {
       'a'..='z' | 'A'..='Z' | '0'..='9' => true,
@@ -64,23 +64,23 @@ impl Parser {
     }
   }
 
-  // Parse a text node.
+  // Parse a text node
   fn parse_text(&mut self) -> dom::Node {
     dom::Node::text(self.consume_while(|c| c != '<'))
   }
 
-  // Parse a single element, including its open tag, contents, and closing tag.
+  // Parse a single element, including its open tag, contents, and closing tag
   fn parse_element(&mut self) -> dom::Node {
-    // Opening tag.
+    // Opening tag
     assert!(self.consume_char() == '<');
     let tag_name = self.parse_tag_name();
     let attributes = self.parse_attributes();
     assert!(self.consume_char() == '>');
 
-    // Contents.
     let children = self.parse_nodes();
+    // Contents
 
-    // Closing tag.
+    // Closing tag
     assert!(self.consume_char() == '<');
     assert!(self.consume_char() == '/');
     assert!(self.parse_tag_name() == tag_name);
@@ -89,7 +89,8 @@ impl Parser {
     return dom::Node::element(tag_name, attributes, children);
   }
 
-  // Parse a single name="value" pair.
+  // Parse a single node
+  // Parse a single name="value" pair
   fn parse_attribute(&mut self) -> (String, String) {
     let name = self.parse_tag_name();
     assert!(self.consume_char() == '=');
@@ -97,7 +98,7 @@ impl Parser {
     return (name, value);
   }
 
-  // Parse a quoted value.
+  // Parse a quoted value
   fn parse_attribute_value(&mut self) -> String {
     let open_quote = self.consume_char();
     assert!(open_quote == '"' || open_quote == '\'');
@@ -106,7 +107,7 @@ impl Parser {
     return value;
   }
 
-  // Parse a list of name="value" pairs, separated by whitespace.
+  // Parse a list of name="value" pairs, separated by whitespace
   fn parse_attributes(&mut self) -> dom::AttributesMap {
     let mut attributes = HashMap::new();
     loop {
@@ -120,7 +121,7 @@ impl Parser {
     return attributes;
   }
 
-  // Parse a sequence of sibling nodes.
+  // Parse a sequence of sibling nodes
   fn parse_nodes(&mut self) -> Vec<dom::Node> {
     let mut nodes = Vec::new();
     loop {
@@ -133,7 +134,7 @@ impl Parser {
     return nodes;
   }
 
-  // Parse an HTML document and return the root element.
+  // Parse an HTML document and return the root element
   pub fn parse(source: String) -> dom::Node {
     let mut nodes = Parser {
       position: 0,
@@ -141,7 +142,7 @@ impl Parser {
     }
     .parse_nodes();
 
-    // If the document contains a root element, just return it. Otherwise, create one.
+    // If the document contains a root element, just return it. Otherwise, create one
     if nodes.len() == 1 {
       nodes.swap_remove(0)
     } else {
