664a1961375766294684eff88b5afd313bacd8f3
Federico Baldini <federico.baldini7@gmail.com>
2023-01-14 09:54:43 +0100

    Update main.rs
    Used the new dom::Node::print_node_tree() function.

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ mod parser;
 
 fn main() {
   let _html_text_dom = dom::Node::text("Hello World!".to_string());
-  let _root_dom_node = parser::Parser::parse("<div><span>Hello World!</span></div>".to_string());
+  let root_dom_node = parser::Parser::parse("<div class='container-1'></div><div class='container-2'><p class='paragraph'>Hello World!</p></div>".to_string());
+  dom::Node::print_node_tree(root_dom_node, 0);
 }
