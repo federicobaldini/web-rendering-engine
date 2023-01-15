bfcdc86aa483134342d38a7a3fc674f5ca6f6940
Federico Baldini <federico.baldini7@gmail.com>
2023-01-15 13:28:10 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod css_parser;
 mod dom;
 mod html_parser;
+mod css;
 
 fn main() {
-  let _html_text_dom = dom::Node::text("Hello World!".to_string());
-  let root_dom_node = html_parser::Parser::parse("<div class='container-1'></div><div class='container-2'><p class='paragraph'>Hello World!</p></div>".to_string());
+  let _html_text_dom: dom::Node = dom::Node::text("Hello World!".to_string());
+  let root_dom_node: dom::Node = html_parser::Parser::parse("<div class='container-1'></div><div class='container-2'><p class='paragraph'>Hello World!</p></div>".to_string());
   dom::Node::print_node_tree(root_dom_node, 0);
+  let _css_stylesheet: css::Stylesheet =
+    css_parser::Parser::parse(".container{width:200px}".to_string());
 }
