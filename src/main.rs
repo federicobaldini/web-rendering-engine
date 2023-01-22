5b2ee0c11d99f6eeaadf384265548bd78f0e0594
Federico Baldini <federico.baldini7@gmail.com>
2023-01-22 13:07:50 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ mod dom;
 mod html_parser;
 mod text_parser;
 
+use crate::css_parser::CSSParser;
+use crate::dom::Node;
+use crate::html_parser::HTMLParser;
+
 fn main() {
-  let _html_text_dom: dom::Node = dom::Node::text("Hello World!".to_string());
-  let root_dom_node: dom::Node = html_parser::HTMLParser::parse("<div class='container-1'></div><div class='container-2'><p class='paragraph'>Hello World!</p></div>".to_string());
-  dom::Node::print_node_tree(root_dom_node, 0);
-  let _css_stylesheet: css::Stylesheet =
-    css_parser::CSSParser::parse(".container{width:200px}".to_string());
+  let _html_text_dom: Node = Node::text("Hello World!".to_string());
+  let root_dom_node: Node = HTMLParser::parse("<div class='container-1'></div><div class='container-2'><p class='paragraph'>Hello World!</p></div>".to_string());
+  Node::print_node_tree(root_dom_node, 0);
+  let _css_stylesheet: css::Stylesheet = CSSParser::parse(".container{width:200px;}".to_string());
 }
