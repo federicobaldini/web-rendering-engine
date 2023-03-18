3352c3be136f3fe176ec242b63b1b91abf44b191
Federico Baldini <federico.baldini7@gmail.com>
2023-03-18 12:03:51 +0100

    Update painting.rs
    Added the DisplayList type and the DisplayCommand enum and its implementation of the trait PartialEq.

--- src/painting.rs
+++ src/painting.rs
@@ -1,2 +1,19 @@
 use crate::css;
 use crate::layout;
+
+type DisplayList = Vec<DisplayCommand>;
+
+#[derive(Debug)]
+enum DisplayCommand {
+  SolidColor(css::Color, layout::Rectangle),
+  // insert more commands here
+}
+
+impl PartialEq for DisplayCommand {
+  fn eq(&self, other: &Self) -> bool {
+    match (self, other) {
+      (DisplayCommand::SolidColor(a, b), DisplayCommand::SolidColor(c, d)) => a == c && b == d,
+      _ => false,
+    }
+  }
+}
