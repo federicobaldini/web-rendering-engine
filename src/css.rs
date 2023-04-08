2e39cdb6f8b1bc6412c8145702609232461b1108
Federico Baldini <federico.baldini7@gmail.com>
2023-04-08 09:53:13 +0200

    Update css.rs
    Added new method to the Color struct implementation.

--- src/css.rs
+++ src/css.rs
@@ -95,7 +95,7 @@ impl fmt::Display for Unit {
   }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Copy, Clone, Debug)]
 pub struct Color {
   red: u8,
   green: u8,
@@ -130,6 +130,22 @@ impl Color {
       blue,
       alpha,
     }
+  }
+
+  pub fn red(&self) -> u8 {
+    self.red
+  }
+
+  pub fn green(&self) -> u8 {
+    self.green
+  }
+
+  pub fn blue(&self) -> u8 {
+    self.blue
+  }
+
+  pub fn alpha(&self) -> u8 {
+    self.alpha
   }
 }
 
